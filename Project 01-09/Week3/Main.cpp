4218087ee47a9461d41e96ce8134d4164b6cfe81
Telluluu <Tellulu1564959850@gmail.com>
2023-04-08 18:26:44 +0800

    Week3还没做完

--- Project 01-09/Week3/Main.cpp
+++ Project 01-09/Week3/Main.cpp
@@ -14,11 +14,14 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
     _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 #endif
 
-    GameApp theApp(hInstance, L"Texture Mapping", 1280, 720);
+    GameApp theApp(hInstance, L"Week3", 1280, 720);
     
     if( !theApp.Init() )
         return 0;
     
     return theApp.Run();
 }
+
+
+
 
